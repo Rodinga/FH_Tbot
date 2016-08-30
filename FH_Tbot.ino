947fb24c49c56d6afd534bcbe059e90be71d77d5
Alex Battarbee <Alex Battarbee>
2016-08-30 21:24:35 +0930

    Indication now works, actual blinking should be looked at.

--- FH_Tbot.ino
+++ FH_Tbot.ino
@@ -1,9 +1,9 @@
 /*
- * 
- * 
- */
 
- 
+
+*/
+
+
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <Ticker.h>
@@ -14,9 +14,9 @@
 // WebPage
 #include "WebPage.h"
 
-extern "C" { 
-   #include "user_interface.h" 
- } 
+extern "C" {
+#include "user_interface.h"
+}
 
 //////////////////////
 // RGB LEDs //
@@ -31,10 +31,10 @@ extern "C" {
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 #define PIN D4
 #define NUMPIXELS      6
-NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(6,D4);//6 [pixels on pin d4]
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(6, D4); //6 [pixels on pin d4]
 int delayval = 500; // delay for half a second
 boolean useBlinkers = true;
-            
+
 
 
 //////////////////////
@@ -57,27 +57,27 @@ const int motorLeftB  = D6;
 const int motorRightA = D3;
 const int motorRightB = D2;
 
-motorController motors(motorLeftA,motorLeftB,motorRightA,motorRightB);
+motorController motors(motorLeftA, motorLeftB, motorRightA, motorRightB);
 
 /*
-// stepper with direction and speed pins, don't use D0 for speed
-const int motorLeftDir  = D2;
-const int motorLeftSpd  = D1;
-const int motorRightDir = D5;
-const int motorRightSpd = D6;
+  // stepper with direction and speed pins, don't use D0 for speed
+  const int motorLeftDir  = D2;
+  const int motorLeftSpd  = D1;
+  const int motorRightDir = D5;
+  const int motorRightSpd = D6;
 
-motorController motors(motorLeftDir,D5,motorLeftSpd,motorRightDir,D4,motorRightSpd); 
+  motorController motors(motorLeftDir,D5,motorLeftSpd,motorRightDir,D4,motorRightSpd);
 */
 /*
-// stepper with 2 motor pins and a enable pin per motor, don't use D0 for speed
-const int motorLeftA    =  D1;
-const int motorLeftB    =  D0;
-const int motorLeftSpd  =  D2;
-const int motorRightA   =  D3;
-const int motorRightB   =  D4;
-const int motorRightSpd =  D5;
+  // stepper with 2 motor pins and a enable pin per motor, don't use D0 for speed
+  const int motorLeftA    =  D1;
+  const int motorLeftB    =  D0;
+  const int motorLeftSpd  =  D2;
+  const int motorRightA   =  D3;
+  const int motorRightB   =  D4;
+  const int motorRightSpd =  D5;
 
-motorController motors(motorLeftA,motorLeftB,motorLeftSpd,motorRightA,motorRightB,motorRightSpd); 
+  motorController motors(motorLeftA,motorLeftB,motorLeftSpd,motorRightA,motorRightB,motorRightSpd);
 */
 WiFiServer server(80);                  // http only https is 443
 WiFiClient client;
@@ -92,7 +92,7 @@ bool HeartBeatRcvd = false;
 
 void Stop(void)
 {
-  motors.update(0,0);
+  motors.update(0, 0);
 }
 
 void CheckHeartBeat(void)
@@ -108,67 +108,69 @@ void CheckHeartBeat(void)
   }
 }
 
-/**
- * Flashes forward LEDs to indicate direction of travel.
- * Should scale to larger LED counts (assuming two rows).
- * Stupid LED Trick mostly.
- * param int indexOfX: x axis on Joystick, left or right.
- * param int indexOfY: y axis on Joystick, forward or back.
- */
-void updateBlinkers(int indexOfX, int indexOfY){
-  //Creates array for pixels
-  RgbColor px[NUMPIXELS];
-  for(int i = 0; i < NUMPIXELS; i++){
-    px[i] = RgbColor(0,0,0);
+int makePositive(int in) {
+  if (in < 0) {
+    return -in;
+  } else {
+    return in;
   }
-  //Pick which variable is greater in magnitude.
-    //If X>Y the result should be 0
-    if((indexOfX % indexOfY) == 0){
-      if(indexOfX < 0 ){
-        //Update Left
-        //px[(NUMPIXELS/2 - 1)] = RgbColor(255,127,0);
-        //px[NUMPIXELS - 1] =  RgbColor(255,127,0);
-        px[2] = RgbColor(80,40,0);
-        px[5] =  RgbColor(80,40,0);
-      }else if (indexOfX > 0){
-        //update Right
-        //px[0] =  RgbColor(255,127,0);
-        //px[(NUMPIXELS/2)] = RgbColor(255,127,0);
-        px[0] =  RgbColor(80,40,0);
-        px[3] = RgbColor(80,40,0);
-      }else{
-        //Equals 0 no indication        
-      }      
-    }else{
-      if(indexOfY < 0 ){
-        //px[(NUMPIXELS/2) - 2] = RgbColor(255,127,0);
-        //px[(NUMPIXELS/2) - 1] = RgbColor(255,127,0);
-        //px[(NUMPIXELS/2)] = RgbColor(255,127,0);
-        px[0] = RgbColor(80,40,0);
-        px[1] = RgbColor(80,40,0);
-        px[2] = RgbColor(80,40,0);
-      }else if (indexOfY > 0){
-        //px[(NUMPIXELS) - 2] = RgbColor(255,127,0);
-        //px[(NUMPIXELS) - 1] = RgbColor(255,127,0);
-        //px[(NUMPIXELS)] = RgbColor(255,127,0);        
-        px[3] = RgbColor(80,40,0);
-        px[4] = RgbColor(80,40,0);
-        px[5] = RgbColor(80,40,0);
-      }else{
-        //Equals 0 no indication
-      }  
-    }
-    
-    for(int i = 0; i< NUMPIXELS; i++){
-      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-      strip.SetPixelColor(i, px[i]); // Orange colour.
-      strip.Show(); // This sends the updated pixel color to the hardware.
-      //delay(delayval); // Delay for a period of time (in milliseconds).
-    }
 }
 
-void setup()
-{
+/**
+   Flashes forward LEDs to indicate direction of travel.
+   Should scale to larger LED counts (assuming two rows).
+   Stupid LED Trick mostly.
+   param int indexOfX: x axis on Joystick, left or right.
+   param int indexOfY: y axis on Joystick, forward or back.
+*/
+void updateBlinkers(int indexOfX, int indexOfY) {
+  //Creates array for pixels
+  RgbColor px[NUMPIXELS];
+  for (int i = 0; i < NUMPIXELS; i++) {
+    px[i] = RgbColor(0, 0, 0);
+  }
+
+  if (makePositive(indexOfX) > 30) {
+    if (indexOfX < 0 ) {
+      //Update Left
+      //px[(NUMPIXELS/2 - 1)] = RgbColor(255,127,0);
+      //px[NUMPIXELS - 1] =  RgbColor(255,127,0);
+      px[2] = RgbColor(80, 40, 0);
+      px[5] =  RgbColor(80, 40, 0);
+    } else if (indexOfX > 0) {
+      //update Right
+      //px[0] =  RgbColor(255,127,0);
+      //px[(NUMPIXELS/2)] = RgbColor(255,127,0);
+      px[0] =  RgbColor(80, 40, 0);
+      px[3] = RgbColor(80, 40, 0);
+    }
+  } else {
+    if (indexOfY < 0 ) {
+      //px[(NUMPIXELS/2) - 2] = RgbColor(255,127,0);
+      //px[(NUMPIXELS/2) - 1] = RgbColor(255,127,0);
+      //px[(NUMPIXELS/2)] = RgbColor(255,127,0);
+      px[0] = RgbColor(80, 40, 0);
+      px[1] = RgbColor(80, 40, 0);
+      px[2] = RgbColor(80, 40, 0);
+    } else if (indexOfY > 0) {
+      //px[(NUMPIXELS) - 2] = RgbColor(255,127,0);
+      //px[(NUMPIXELS) - 1] = RgbColor(255,127,0);
+      //px[(NUMPIXELS)] = RgbColor(255,127,0);
+      px[3] = RgbColor(80, 40, 0);
+      px[4] = RgbColor(80, 40, 0);
+      px[5] = RgbColor(80, 40, 0);
+    }
+
+  }
+  for (int i = 0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    strip.SetPixelColor(i, px[i]); // Orange colour.
+    strip.Show(); // This sends the updated pixel color to the hardware.
+    //delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+}
+void setup() {
   //system_update_cpu_freq(160);        // set cpu to 160MHZ !
   initHardware();
   setupWiFi();
@@ -178,50 +180,50 @@ void setup()
   //motors.setSteeringSensitivity(0.5);  // this setting is optional
   motors.setPWMFrequency(50);           // this setting is optional, depending on power supply and H-Bridge this option will alter motor noise and torque.
   //motors.setMinimumSpeed(0.25);         // this setting is optional, default is 0.1(10%) to prevent motor from stalling at low speed
-    
+
   strip.Begin();
   strip.Show(); // Initialize all pixels to 'off'
-     //////////////////////////
- for(int i=0;i<NUMPIXELS;i++){
+  //////////////////////////
+  for (int i = 0; i < NUMPIXELS; i++) {
 
     // strip.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.SetPixelColor(i, RgbColor(0,40,0)); // Moderately bright green color.
+    strip.SetPixelColor(i, RgbColor(0, 40, 0)); // Moderately bright green color.
 
   }
   strip.Show(); // This sends the updated pixel color to the hardware.
   //delay(delayval); // Delay for a period of time (in milliseconds).
-    
 
-  
+
+
   /////////////////////////////////////////////
-  
-  }
+
+}
 
 void loop()
 {
   // time dependant functions here
-   ping.run();
-   if (ping.gotTemperature()) Serial.printf("Temperature: %d C \r\n", ping.getTemperature());
-   if (ping.gotDistance()) Serial.printf("distance: %d mm \r\n", ping.getDistance());
-   dnsServer.processNextRequest();
+  ping.run();
+  if (ping.gotTemperature()) Serial.printf("Temperature: %d C \r\n", ping.getTemperature());
+  if (ping.gotDistance()) Serial.printf("distance: %d mm \r\n", ping.getDistance());
+  dnsServer.processNextRequest();
 
-   // client functions here
-   if (clientStopped){
+  // client functions here
+  if (clientStopped) {
     client = server.available();
     clientStopped = false;
     nextClientTimeout = millis() + clientTimeout;
-   }else{
-    if (!client.connected() || millis() > nextClientTimeout){
+  } else {
+    if (!client.connected() || millis() > nextClientTimeout) {
       client.stop();
       clientStopped = true;
     }
-   }
-   
+  }
+
   if (!client.available())
   {
     return;
   }
-  
+
   // Read the first line of the request
   String req = client.readStringUntil('\r');
   //Serial.println(req);
@@ -229,16 +231,16 @@ void loop()
   client.flush();
   int indexOfX = req.indexOf("/X");
   int indexOfY = req.indexOf("/Y");
-  if (indexOfX != -1 && indexOfY != -1){
-    
+  if (indexOfX != -1 && indexOfY != -1) {
+
     String xOffset = req.substring(indexOfX + 2, indexOfX + 8);
     int dX = xOffset.toInt();
     String yOffset = req.substring(indexOfY + 2, indexOfY + 8);
     int dY = yOffset.toInt();
-    
-    motors.update(dX,dY);
 
-        
+    motors.update(dX, dY);
+
+
     //Serial.print(F("DX: "));
     //Serial.print(dX);
     //Serial.print(F(" DY: "));
@@ -250,48 +252,48 @@ void loop()
 
     //Blinker Addon
     //Only operates if enabled in RGB options above
-    if(useBlinkers == true){
-      updateBlinkers(dX,dY);
+    if (useBlinkers == true) {
+      updateBlinkers(dX, dY);
     }
 
-  }else{
-        if (req.indexOf("GET / HTTP/1.1") != -1){
-            Serial.println(F("Sending Page"));
-            client.write_P(HTML_text,strlen_P(HTML_text));
-            delay(1);                   // to improve compatability with some browsers
-          }
-        }
+  } else {
+    if (req.indexOf("GET / HTTP/1.1") != -1) {
+      Serial.println(F("Sending Page"));
+      client.write_P(HTML_text, strlen_P(HTML_text));
+      delay(1);                   // to improve compatability with some browsers
+    }
+  }
 }
 
 void setupWiFi()
 {
   WiFi.mode(WIFI_AP);
- 
+
   // Create a unique name by appending the MAC address to the AP Name
 
   AP_Name = AP_Name + " " + WiFi.softAPmacAddress();
   char AP_NameChar[AP_Name.length() + 1];
-  AP_Name.toCharArray(AP_NameChar,AP_Name.length() + 1);
+  AP_Name.toCharArray(AP_NameChar, AP_Name.length() + 1);
 
   // setup AP, start DNS server, start Web server
 
-  int channel = random(1,13);
+  int channel = random(1, 13);
   const byte DNS_PORT = 53;
   IPAddress subnet(255, 255, 255, 0);
   IPAddress apIP(192, 168, 1, 1);
   WiFi.softAPConfig(apIP, apIP, subnet);
-  if (enableCompatibilityMode){
+  if (enableCompatibilityMode) {
     wifi_set_phy_mode(PHY_MODE_11B);    // Note: ESP8266 soft-AP only support bg.
     const char *pw = "";
     WiFi.softAP(AP_NameChar, pw , channel , 0 );
-  }else{
+  } else {
     wifi_set_phy_mode(PHY_MODE_11N);
     WiFi.softAP(AP_NameChar, password , channel , 0 );
   }
   dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
   dnsServer.start(DNS_PORT, "FHTbot.com", apIP);//must use '.com, .org etc..' and cant use '@ or _ etc...' ! . Use "*" to divert all **VALID** names
   server.begin();
-  
+
   //WiFi.printDiag(Serial);
 }
 
